d9ba5afabc408cda6dae933a374b316ac7ac917b
Tobias Wallin <tobias.wallin.uppsala@gmail.com>
2022-07-11 22:59:07 +0200

    Dec2Bin added

--- Conversions.py
+++ Conversions.py
@@ -1,3 +1,4 @@
+import math
 from math import pi
 
 # ==================================================================
@@ -15,6 +16,19 @@ def Deg2Rad( deg ):
     """Convert an angular value from Degree to Radian """
     return deg * pi / 180
 
+def Dec2Bin( numDec : int ):
+    """Convert an numeric value from decimal (base 10) to binary (base 2) """
+    
+    # End of recursion
+    if numDec == 0:
+        return ""
+
+    # Divide value by 2 and recurse
+    # Append a '1' if the current value is even, or '0' otherwise
+    return Dec2Bin( numDec // 2 ) + str( numDec % 2)
+
+
 ########### This is the main entry point #########
 
-print( Deg2Rad(180) )
+#print( Deg2Rad(180) )
+print( Dec2Bin(345) )
