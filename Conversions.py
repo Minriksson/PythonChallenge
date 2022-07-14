393fbd032419b67b9452d5f2f89a0f96677f5a1d
Tobias Wallin <tobias.wallin.uppsala@gmail.com>
2022-07-14 21:54:42 +0200

    Dec2Hex added

--- Conversions.py
+++ Conversions.py
@@ -16,7 +16,7 @@ def Deg2Rad( deg ):
     """Convert an angular value from Degree to Radian """
     return deg * pi / 180
 
-def Dec2Bin( numDec : int ):
+def Dec2Binx( numDec : int ):
     """Convert an numeric value from decimal (base 10) to binary (base 2) """
     
     # End of recursion
@@ -25,10 +25,58 @@ def Dec2Bin( numDec : int ):
 
     # Divide value by 2 and recurse
     # Append a '1' if the current value is even, or '0' otherwise
-    return Dec2Bin( numDec // 2 ) + str( numDec % 2)
+    return Dec2Binx( numDec // 2 ) + str( numDec % 2)
+
+def Dec2Bin( numDec : int ):
+    """Convert an numeric value from decimal (base 10) to binary (base 2) """
+    
+    # Start with an empty string
+    strBin = ""
+
+    while numDec > 0:
+        # Divide the value with the base (e.g. 2) and use the reminder as the next character
+        strBin = str( numDec % 2) + strBin
+        # Divide remaining value by the base (e.g. 2) and continue loop
+        numDec = numDec // 2
+
+    return strBin
+
+def Dec2Hex( numDec : int ):
+    """Convert an numeric value from decimal (base 10) to hexadecimal (base 16) """
+    
+    dictHex = {
+        0:"0",
+        1:"1",
+        2:"2",
+        3:"3",
+        4:"4",
+        5:"5",
+        6:"6",
+        7:"7",
+        8:"8",
+        9:"9",
+        10:"A",
+        11:"B",
+        12:"C",
+        13:"D",
+        14:"E",
+        15:"F"
+    }
+    # Start with an empty string
+    strHex = ""
+
+    while numDec > 0:
+        # Divide the value with the base (e.g. 16) and use the reminder as the next character
+        strHex = dictHex[numDec % 16] + strHex
+        # Divide remaining value by the base (e.g. 16) and continue loop
+        numDec = numDec // 16
+
+    return strHex
 
 
 ########### This is the main entry point #########
 
 #print( Deg2Rad(180) )
 print( Dec2Bin(345) )
+print( Dec2Binx(345) )
+print( Dec2Hex(255) )
